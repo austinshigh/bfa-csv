e2a5388d0d37d9945316a91dab87e68857469cd2
Austin <austinstewarthigh@gmail.com>
2021-11-17 17:12:49 -0500

    changed description

--- src/App.js
+++ src/App.js
@@ -10,17 +10,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome.
-            The textbox below extracts the numbers contained on each line of an input text,
-            and returns a comma separated list of the numbers.
+            <h1>Welcome.</h1>
+            {/*<h4>*/}
+            {/*The textbox below accepts alphanumeric characters,<br></br> parses out the numbers on each line,<br></br> and generates a comma separated list.*/}
+            {/*</h4>*/}
+            <div id="description">
+            <h5>
+                <p>Enter this text:<br></br>"01010101randomText<br></br>randomText0000000<br></br>1111111111moreRandomText"</p>
+            </h5>
 
-            Enter this text:
-            "01010101randomText
-            randomText0000000
-            11111111111moreRandomText"
-
-            And you will receive, free of charge, this text:
-            "01010101, 0000000, 11111111111"
+            <h5>
+                And receive, free of charge, this text:
+                <p>"01010101, 0000000, 11111111111"</p>
+            </h5>
+            </div>
         </p>
          <Parse/>
       </header>
